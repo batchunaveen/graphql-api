dd9498163f8da09b0ca4b6e16167c2639af3da15
Naveen <batchunaveen@users.noreply.github.com>
2021-12-22 10:27:39 -0500

    UI
    Adding UI Component

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,2 +1,42 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
+
+function fetchingData() {
+    var schema = buildSchema(`
+        type Cards {
+        id: int,
+        type: int,
+        from: string,
+        back: string
+        }
+        
+    `)
+
+      var query = `query { 
+        cards {  
+           id    
+           type     
+           front     
+           back     
+           known    
+           } 
+        }`;
+      fetch('/graphql', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+          },
+          body: JSON.stringify({
+              query
+          })
+      })
+          .then(response => response.json())
+          .then(
+              ({data}) =>
+                `<p>Id : ${data.id} </p>
+                 <p>Type: ${data.type} </p>
+                 <p>Front: ${data.front} </p>`
+            )
+          .then(text => (document.body.innerHTML = text))
+          .catch( error => console.log(error));
+  }
+  fetchingData();
